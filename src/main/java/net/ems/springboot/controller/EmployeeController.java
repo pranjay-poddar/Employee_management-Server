cbad7af2ffc0c1b0faadbbe6ddd1d08442e86463
pranjay-poddar <pranjaypoddar@outlook.com>
2021-12-05 01:06:31 +0530

    added gettters and setters

---
+++ src/main/java/net/ems/springboot/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package net.ems.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.ems.springboot.repository.EmployeeRepository;
+import net.ems.springboot.model.Employee;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	//get all employees
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+}
---
+++ src/main/java/net/ems/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package net.ems.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.ems.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
